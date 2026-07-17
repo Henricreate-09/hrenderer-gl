751237a4148c37be24afbfecaf05364b8807b7a2
SHenrique <87613130+Henricreate-09@users.noreply.github.com>
2026-07-17 16:33:15 -0300

    Make GLM aliases

--- Src/Core/Defs.hpp
+++ Src/Core/Defs.hpp
@@ -2,5 +2,11 @@
 
 #include <iostream>
 #include <vector>
+#include <glm/glm.hpp>
 
 using String = std::string;
+
+using Vec2 = glm::vec2;
+using Vec3 = glm::vec3;
+using Vec4 = glm::vec4;
+using Mat4 = glm::mat4;
